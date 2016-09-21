2667e27f06f85c90f434d69574f78a7b450df1fc
Kadin Hatchell <kadinahatchell@gmail.com>
2016-09-21 08:22:24 -0600

    Created new View

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.blueColor()
+DynamicView.layer.cornerRadius = 100
+DynamicView.layer.borderWidth = 2
+DynamicView
+
+var SuperView = UIView(frame: CGRectMake(500, 1000, 500, 500))
+SuperView.backgroundColor=UIColor.redColor()
+SuperView.layer.cornerRadius = 450
+SuperView.layer.borderWidth = 10
+SuperView
